993e8569065d8e661ff170c08e27c86cb0dd73cf
TamoToo <83399518+TamoToo@users.noreply.github.com>
2026-09-23 14:36:31 +0200

    Solve tfidf-vectorizer

--- tfidf-vectorizer/tfidf-vectorizer.py
+++ tfidf-vectorizer/tfidf-vectorizer.py
@@ -12,26 +12,10 @@ def tfidf_vectorizer(documents: list[str]) -> dict:
     vocabulary = sorted(list({word for doc in doc_splits for word in doc}))
     
     df = Counter(word for doc in doc_splits for word in set(doc))
-
     tfidf = [
         [(doc.count(word) / len(doc)) * math.log(n / df[word]) for word in vocabulary]
         for doc in doc_splits
     ]
-
-    # tf = []
-    # for doc in documents:
-    #     tmp = []
-    #     d = len(doc.split())
-    #     for word in vocabulary:
-    #         tmp.append(doc.count(word) / d)
-    #     tf.append(tmp)
-          
-    # idf = {t: math.log(n / d) for t,d in df.items()}
-    # tfidf = tf
-    # for i, doc in enumerate(documents):
-    #     for j, word in enumerate(vocabulary):
-    #         tfidf[i][j] *= idf[word]
-
     return {
         "tfidf_matrix": np.asarray(tfidf),
         "vocabulary": vocabulary
