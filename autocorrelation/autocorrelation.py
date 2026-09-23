105f9feadd0ee1cce1f2698290515f76de2de3e3
TamoToo <83399518+TamoToo@users.noreply.github.com>
2026-09-23 19:45:21 +0200

    Solve autocorrelation

--- autocorrelation/autocorrelation.py
+++ autocorrelation/autocorrelation.py
@@ -3,15 +3,16 @@ def autocorrelation(series: list, max_lag: int) -> list:
     Returns normalized autocorrelation from lag zero through max_lag.
     """
     # Write code here
-    mean = sum(series) / len(series)
+    n = len(series)
+    mean = sum(series) / n
     variance = sum([(x - mean)**2 for x in series])
     result = [1.0]
     for i in range(1, max_lag + 1):
         if variance == 0:
             result.append(0.0)
             continue
-        auto_cor = (1 / variance) * sum([(series[k] - mean) * (series[k+i] - mean) for k in range(len(series) - i)])
-        result.append(round(auto_cor, 6))
+        cov = (1 / variance) * sum([(series[k] - mean) * (series[k+i] - mean) for k in range(n - i)])
+        result.append(round(cov, 6))
 
     return result
     
